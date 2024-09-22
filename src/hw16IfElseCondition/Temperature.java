84f4efee092c446735c71b7e73798a137ff52fc0
ifzal-2024 <ifzal.java2024@gmail.com>
2024-09-22 02:01:00 -0500

    HW15IfElseCondition, HW16IfElseCondition

---
+++ src/hw16IfElseCondition/Temperature.java
@@ -0,0 +1,28 @@
+package hw16IfElseCondition;
+
+import java.util.Scanner;
+
+public class Temperature {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please entyer Today's Temperature");
+		int todaysTemperature = scanner.nextInt();
+
+		if (todaysTemperature < 32) {
+			System.out.println("Its Freesing outside");
+		} else if (todaysTemperature < 55) {
+			System.out.println("Its pleasant outside");
+		} else if (todaysTemperature < 73) {
+			System.out.println("Its getting warmer outside");
+		} else if (todaysTemperature < 101) {
+			System.out.println("Its hot outside");
+		} else if (todaysTemperature >= 101) {
+			System.out.println("Its very hot outside");
+		} else {
+			System.out.println("Please put a valid temperature");
+		}
+
+		scanner.close();
+	}
+
+}
