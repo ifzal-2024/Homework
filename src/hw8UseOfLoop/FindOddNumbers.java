9684aafa8cb318360fc43bae203ab4444774b7e3
ifzal-2024 <ifzal.java2024@gmail.com>
2024-10-16 00:14:13 -0500

    Hw08UseOfLoop Odd Numbers

---
+++ src/hw8UseOfLoop/FindOddNumbers.java
@@ -0,0 +1,13 @@
+package hw8UseOfLoop;
+
+public class FindOddNumbers {
+	public static void main(String[] args) {
+		for(int i=-20;i<=50;i++) {
+			if (i%2==1) {
+				System.out.println("Odd Number: "+ i);
+			}
+			
+		}
+	}
+
+}
